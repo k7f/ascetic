134d8c71c0185f5aa70b3033c1379718a358ff23
krzYszcz <krzYszcz@ubu13.local>
2020-04-03 22:01:51 +0200

    [vis] Reduce cloning in the `tweener` module.

--- vis/src/tweener.rs
+++ vis/src/tweener.rs
@@ -12,77 +12,107 @@ pub trait Tweenable: Steppable + Sized {
     /// Compute a set of passing positions between `self` and `other`
     /// extremes.
     ///
-    /// The value returned is assumed to be a `Vec` of no more than
-    /// `max_subdivision` tweens equally spaced in tween space,
-    /// i.e. without the application of time-mapping (easing).  All
-    /// distances are required to be the same: from `self` to first
-    /// element, from last element to `other`, and between any two
-    /// consecutive elements.
+    /// The value returned is expected to be a `Vec` of no less than 2
+    /// and no more than `max_inner` + 2 tweens equally spaced in
+    /// tween space, i.e. without the application of time-mapping
+    /// (easing).  All distances between two consecutive elements are
+    /// assumed to be the same.  The first element must be equal to
+    /// `self`, the last element must be equal to `other`.
     ///
-    /// Default implementation returns an empty `Vec`.  Reimplement,
-    /// if there is a need for expensive high quality interpolation
-    /// (e.g. hsv-based color interpolation) that shouldn't be
-    /// performed within each call to `Steppable::step()`.
+    /// Default implementation returns a two-element `Vec` containing
+    /// just the two extremes.  Reimplement, if there is a need for
+    /// expensive high quality interpolation (e.g. hsv-based color
+    /// interpolation) that shouldn't be performed within each call to
+    /// `Steppable::step()`.
     #[allow(unused_variables)]
-    fn breakdown(&self, other: &Self, max_subdivision: usize) -> Vec<Self> {
-        Vec::new()
+    fn breakdown(self, other: Self, max_inner: usize) -> Vec<Self> {
+        vec![self, other]
     }
 }
 
-// FIXME clone only in the ctor
 #[derive(Clone, Debug)]
 pub struct Tweener<T: Tweenable + Clone> {
-    start:    T,
-    targets:  Vec<T>,
-    total:    f64,
-    position: f64,
-    value:    T,
+    breakpoints:  Vec<T>,
+    num_segments: f64,
+    position:     f64,
+    value:        T,
 }
 
 impl<T: Tweenable + Clone> Tweener<T> {
-    pub fn new(start: T, stop: T, max_subdivision: usize) -> Self {
+    pub fn new(start: T, stop: T, max_inner: usize) -> Self {
         // eprintln!("Tweener {:?} -> {:?}", start, stop);
 
-        let mut targets = start.breakdown(&stop, max_subdivision);
-        targets.push(stop);
-
-        let total = targets.len() as f64;
-        let position = 0.0;
         let value = start.clone();
+        let breakpoints = start.breakdown(stop, max_inner);
+        let mut tweener = Tweener { breakpoints, num_segments: 0.0, position: 0.0, value };
 
-        Tweener { start, targets, total, position, value }
+        tweener.initialize();
+
+        tweener
     }
 
-    pub fn restart(&mut self) {
+    fn initialize(&mut self) {
+        if self.breakpoints.is_empty() {
+            self.breakpoints = vec![self.value.clone()];
+        }
+
+        let mut num_segments = self.breakpoints.len();
+
+        if num_segments == 1 {
+            self.breakpoints.push(self.breakpoints[0].clone());
+        } else {
+            num_segments -= 1;
+        }
+
+        self.num_segments = num_segments as f64;
         self.position = 0.0;
-        self.value = self.start.clone();
+    }
+
+    pub fn reverse(&mut self) {
+        self.position = 0.0;
+        self.value.clone_from(self.breakpoints.last().unwrap());
+        self.breakpoints.reverse();
+    }
+
+    pub fn restart(&mut self, new_stop: T, max_inner: usize) {
+        if let Some(new_start) = self.breakpoints.pop() {
+            self.value.clone_from(&new_start);
+            self.breakpoints = new_start.breakdown(new_stop, max_inner);
+        } else {
+            self.value.clone_from(&new_stop);
+            self.breakpoints = vec![new_stop; 2];
+        }
+
+        self.initialize();
     }
 
     /// The `amount` is expected to be a strictly positive number such
     /// that the total amount accumulated across any sequence of calls
-    /// to `tween_on()` until a call to `restart()` is less than 1.
-    pub fn tween_on(&mut self, amount: f64) -> &T {
+    /// to `tween_on()` until a call to `restart()` is less than or
+    /// equal to 1.
+    pub fn tween_on(&mut self, amount: f64) -> Option<&T> {
         if amount > 0.0 {
             let position = self.position + amount;
 
             if position < 1.0 {
-                let findex = (position * self.total).trunc();
-                let target = &self.targets[findex as usize];
-                let target_position = (findex + 1.0) / self.total;
+                let findex = (position * self.num_segments).trunc() + 1.0;
+                let target = &self.breakpoints[findex as usize];
+                let target_position = findex / self.num_segments;
                 // FIXME if possible, set self.value to pre-target and
                 // increase self.position accordingly.
 
                 self.value.step(target, amount / (target_position - self.position));
                 self.position = position;
+
+                Some(&self.value)
             } else {
-                let target = self.targets.last().unwrap();
-
-                self.value = target.clone();
                 self.position = 1.0;
-            }
-        }
 
-        &self.value
+                self.breakpoints.last()
+            }
+        } else {
+            None
+        }
     }
 }
 
@@ -134,6 +164,7 @@ impl Easing for LinearEasing {
 
 trait AsRgba {
     fn as_rgba(&self) -> (u32, u32, u32, u32);
+    fn as_rgba_f64(&self) -> (f64, f64, f64, f64);
 }
 
 impl AsRgba for Color {
@@ -142,16 +173,27 @@ impl AsRgba for Color {
 
         ((rgba >> 24) & 255, (rgba >> 16) & 255, (rgba >> 8) & 255, rgba & 255)
     }
+
+    fn as_rgba_f64(&self) -> (f64, f64, f64, f64) {
+        let rgba = self.as_rgba_u32();
+
+        (
+            ((rgba >> 24) & 255) as f64,
+            ((rgba >> 16) & 255) as f64,
+            ((rgba >> 8) & 255) as f64,
+            (rgba & 255) as f64,
+        )
+    }
 }
 
 impl Steppable for Color {
     fn step(&mut self, target: &Self, amount: f64) {
-        let (r0, g0, b0, a0) = self.as_rgba();
-        let (r1, g1, b1, a1) = target.as_rgba();
+        let (r0, g0, b0, a0) = self.as_rgba_f64();
+        let (r1, g1, b1, a1) = target.as_rgba_f64();
 
         #[inline]
-        fn lerp(v0: u32, v1: u32, amount: f64) -> u8 {
-            let v = (v0 as f64 + (v1 as f64 - v0 as f64) * amount).round() as i32;
+        fn lerp(v0: f64, v1: f64, amount: f64) -> u8 {
+            let v = (v0 + (v1 - v0) * amount).round() as i32;
             let v = if v < 0 {
                 0
             } else if v > 255 {
@@ -173,16 +215,24 @@ impl Steppable for Color {
 }
 
 impl Tweenable for Color {
-    fn breakdown(&self, other: &Self, max_subdivision: usize) -> Vec<Self> {
-        if max_subdivision > 0 {
-            let mut pp = self.clone();
+    fn breakdown(self, other: Self, max_inner: usize) -> Vec<Self> {
+        if max_inner > 0 {
+            let mut breakpoints = vec![self.clone()];
+            let num_segments = (max_inner + 1) as f64;
 
-            // FIXME use hsv
-            pp.step(other, 0.5);
+            for i in 0..max_inner {
+                let mut inner = self.clone();
 
-            vec![pp]
+                // FIXME use hsv
+                inner.step(&other, (i + 1) as f64 / num_segments);
+                breakpoints.push(inner);
+            }
+
+            breakpoints.push(other);
+
+            breakpoints
         } else {
-            Vec::new()
+            vec![self, other]
         }
     }
 }
@@ -198,24 +248,24 @@ impl Steppable for Stroke {
 }
 
 impl Tweenable for Stroke {
-    fn breakdown(&self, other: &Self, max_subdivision: usize) -> Vec<Self> {
-        let brush = self.get_brush().breakdown(other.get_brush(), max_subdivision);
-        let count = brush.len();
+    fn breakdown(self, other: Self, max_inner: usize) -> Vec<Self> {
+        let brush = self.get_brush().clone().breakdown(other.get_brush().clone(), max_inner);
+        let num_breakpoints = brush.len();
 
-        if count > 0 {
+        if num_breakpoints > 2 {
             let w0 = self.get_width();
             let w1 = other.get_width();
-            let n = (count + 1) as f64;
+            let num_segments = (num_breakpoints - 1) as f64;
 
             brush
                 .into_iter()
                 .enumerate()
                 .map(|(i, b)| {
-                    Stroke::new().with_brush(b).with_width(w0 + (w1 - w0) * ((i + 1) as f64 / n))
+                    Stroke::new().with_brush(b).with_width(w0 + (w1 - w0) * (i as f64 / num_segments))
                 })
                 .collect()
         } else {
-            Vec::new()
+            vec![self, other]
         }
     }
 }
@@ -235,15 +285,15 @@ impl Steppable for Fill {
 }
 
 impl Tweenable for Fill {
-    fn breakdown(&self, other: &Self, max_subdivision: usize) -> Vec<Self> {
+    fn breakdown(self, other: Self, max_inner: usize) -> Vec<Self> {
         match self {
-            Fill::Color(c0) => match other {
+            Fill::Color(ref c0) => match other {
                 Fill::Color(c1) => {
-                    c0.breakdown(c1, max_subdivision).into_iter().map(Fill::Color).collect()
+                    c0.clone().breakdown(c1, max_inner).into_iter().map(Fill::Color).collect()
                 }
-                _ => Vec::new(), // FIXME
+                _ => vec![self, other], // FIXME
             },
-            _ => Vec::new(), // FIXME
+            _ => vec![self, other], // FIXME
         }
     }
 }
